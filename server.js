89f2f47cc21abc7e9468dc56f4de6fff5cbc9e9c
jpmare29 <jpmmare@yahoo.com>
2022-05-15 16:57:52 -0400

    created queries in js

--- server.js
+++ server.js
@@ -11,17 +11,42 @@ app.use(express.json());
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // Your MySQL username,
       user: 'root',
-      // Your MySQL password
       password: process.env.DB_PSWRD,
       database: 'election'
     },
     console.log('Connected to the election database.')
   );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+  VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 //default for unspecified requests
