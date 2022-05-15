b7d83da9561b91e30501578717ad1314767dafa4
jpmare29 <jpmmare@yahoo.com>
2022-05-15 14:45:36 -0400

    started mysql2

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 //middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: process.env.DB_PSWRD,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default for unspecified requests
 app.use((req, res) => {
     res.status(404).end();
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
